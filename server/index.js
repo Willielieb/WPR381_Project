c5d75a0c7b200ef75f43fe84031cb4064772e199
TristenSmallman <66869391+TristenSmallman@users.noreply.github.com>
2020-08-12 14:22:50 +0200

    ApiFix
    Change in how the api searches without the helper function

--- server/index.js
+++ server/index.js
@@ -4,33 +4,29 @@ const app = express();
 const http = require('http');
 const url = require('url');
 const { queryByRole } = require("@testing-library/react");
+const { cursorTo } = require("readline");
+const request = require('request-promise');
+const { Console } = require("console");
 const port = 3001;
 
 var weatherdata;
 var lat = 5.6037
 var lon = 0.1870
 
+APPID = 'ba3e8d5cc30f1c88709d50ab01fbe17',
 
-const helper = new OpenWeatherMapHelper(
-    {
-        APPID: '891395a968d64d9998247fdec85650e6',
-        units: 'metric'
-    }
-);
+
 
 app.get('/weather/api', (req, res) => {
     const queryObject = url.parse(req.url, true).query;
-    queryObject.lat = lat;
-    queryObject.lon = lat;
-    helper.getCurrentWeatherByGeoCoordinates(lat, lon, (err, currentWeather) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            //console.log(currentWeather);
-            res.send(currentWeather);
-        }
-    });
+    //queryObject.lat = lat;
+    //queryObject.lon = lat;
+    request(`https://api.openweathermap.org/data/2.5/onecall?lat=30.489772&lon=-99.771335&exclude=hourly,daily&units=metric&appid=891395a968d64d9998247fdec85650e6`).then(
+        response=>{res.send(response)}).catch(err=>{console.log(err)})
+        
+    
+    
+
 });
 
 app.listen(port, () => {
