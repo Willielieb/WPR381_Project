4db048c7b17198b0146dbc19dfbedbb80c772397
Willem Liebenberg <willem.willeml.liebenberg@gmail.com>
2020-08-12 20:19:09 +0200

    fixed lnglat finding and updated server code

--- server/index.js
+++ server/index.js
@@ -1,25 +1,29 @@
 
 const express = require('express');
-const app = express();
-const http = require('http');
 const url = require('url');
-const { queryByRole } = require("@testing-library/react");
-const { cursorTo } = require("readline");
 const request = require('request-promise');
-const { Console } = require("console");
+
+
 const port = 3001;
-
-var lat;
-var lon;
-
-APPID = 'ba3e8d5cc30f1c88709d50ab01fbe17';
-
-
+const app = express();
+const key = '891395a968d64d9998247fdec85650e6';
 
 app.get('/weather/api', (req, res) => {
     const queryObject = url.parse(req.url, true).query;
-    request(`https://api.openweathermap.org/data/2.5/onecall?lat=${queryObject.lat}&lon=${queryObject.lon}&exclude=hourly,daily&units=metric&appid=891395a968d64d9998247fdec85650e6`).then(
-        response=>{res.send(response)}).catch(err=>{console.log(err)})});
+    var query = {
+        uri: 'https://api.openweathermap.org/data/2.5/onecall',
+        qs: {
+            lat: queryObject.lat,
+            lon: queryObject.lon,
+            exclude: 'hourly',
+            units: 'metric',
+            appid: key
+        }
+    }
+    request(query)
+        .then(response => res.send(response))
+        .catch(err => console.log(err));
+});
 
 app.listen(port, () => {
     console.log(`listening at ${port}`)
