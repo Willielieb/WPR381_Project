02f0fe226d7685a724ac3836f645a102b934902e
TristenSmallman <66869391+TristenSmallman@users.noreply.github.com>
2020-08-14 20:31:01 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 
 const express = require('express');
 const url = require('url');
-const request = require('request-promise')
+const request = require('request-promise');
+const { response } = require('express');
 const port = 3001;
 const app = express();
 const key = '891395a968d64d9998247fdec85650e6';
-var forecast;
+var forecastdata=[];
+
 app.get('/weather/api', (req, res) => {
     const queryObject = url.parse(req.url, true).query
     var query = {
@@ -19,29 +21,31 @@ app.get('/weather/api', (req, res) => {
         }
     }
 
-    request(query)
-        .then(response => res.send(response))
-        .catch(err => console.log(err));
-            forecastdata={
-            "dt": response.daily.dt,
-            "sunrise": response.daily.sunrise,
-            "sunset": response.daily.sunset,
-            "temp": response.daily.temp.day,
-            "feels_like": response.daily.feels_like.day,
-            "pressure": response.daily.pressure,
-            "humidity": response.daily.humidity,
-            "dew_point": response.daily.dew_point,
-            "wind_speed": response.daily.wind_speed,
-            "wind_deg": response.daily.wind_speed,
-            "rain":response.daily.rain,
-            "rainmain":response.daily.weather.main,
-            "raindescription":response.daily.weather.raindescription,
-            "weathericon":response.daily.weather.icon,
-            "cloud":response.daily.clouds,
-        };
+        request(query)
+        .then(response => {res.send(response);
+            var rdaily = response.daily;
+            for(item in rdaily){
+                forecastdata.push({
+                    "dt": item.dt,
+                    "sunrise": item.sunrise,
+                    "sunset": item.sunset,
+                    "temp": item.temp.day,
+                    "feels_like": item.feels_like.day,
+                    "pressure": item.pressure,
+                    "humidity": item.humidity,
+                    "dew_point": item.dew_point,
+                    "wind_speed": item.wind_speed,
+                    "wind_deg": item.wind_speed,
+                    "rain": item.rain,
+                    "rainmain":item.weather.main,
+                    "raindescription":item.weather.raindescription,
+                    "weathericon":item.weather.icon,
+                    "cloud":item.clouds,
+                })
+            }}).catch(err => console.log(err));
 });
 
-app.get('/weather/forecast', (req, res) => {res.send(forecast);});
+app.get('/weather/api/forecast', (req, res) => {res.send(forecastdata);});
 
 app.listen(port, () => {
     console.log(`listening at ${port}`)
