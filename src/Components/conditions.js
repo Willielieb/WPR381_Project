8ee4b75acc20b589a7fdd668ac5474e4c44b80b2
PhilNotFromTheFuture <mccarterphilip@gmail.com>
2020-08-16 07:16:58 +0200

    Map Code
    Here is the code I found that may (or may not) return a map using coordinates. I am unable to acquire an API key from Google, so I haven't tested it.

--- src/Components/conditions.js
+++ src/Components/conditions.js
@@ -86,6 +86,50 @@ class Conditions extends Component {
             )
         }
     }
+	//Here is code that is suspected to return a map. using coordinates.
+	
+	//npm install --save google-map-react
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper, Marker  } from 'google-maps-react';
+
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+class Demo1 extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "React"
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Map
+          google={this.props.google}
+          zoom={14}
+          style={mapStyles}
+          initialCenter={{
+            lat: YOUR_LATITUDE,
+            lng: YOUR_LONGITUDE
+          }}
+        >
+         <Marker
+          onClick={this.onMarkerClick}
+          name={'This is test name'}
+        />
+        </Map>
+      </div>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'API_KEY'
+})(Demo1);
 }
 
 export default Conditions;
